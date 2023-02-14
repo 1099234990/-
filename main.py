2c8747d5475815f47ad682c7be8469fbb6ee3fe5
西米 <111104984+1099234990@users.noreply.github.com>
2023-02-14 15:09:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_weather():
   if city is None:
     print('请设置城市')
     return None
-  url = "http://apis.juhe.cn/simpleWeather/query?city=深圳&key=2a2fa336071b3f88bc4f7365e91786d7"
+  url = "http://apis.juhe.cn/simpleWeather/query?key=2a2fa336071b3f88bc4f7365e91786d7&city=" + city
   # OpenRefactory Warning: The 'requests.get' method does not use any 'timeout' threshold which may cause program to hang indefinitely.
   res = requests.get(url).json()
   if res is None:
