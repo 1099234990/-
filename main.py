7613695745b7cdd333a6dbe8b3c5ed024ce123d8
西米 <111104984+1099234990@users.noreply.github.com>
2023-02-14 15:11:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_weather():
   res = requests.get(url).json()
   if res is None:
     return None
-  weather = result['result']['realtime']['info']
+  weather = res['result']['realtime']['info']
   return weather
 
 
