62bca2a9cee2c41cb1fed7d38c353e86c4dbf797
西米 <111104984+1099234990@users.noreply.github.com>
2023-02-15 11:14:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,10 @@ if template_id is None:
 
 # weather 直接返回对象，在使用的地方用字段进行调用。
 def get_weather():
-    url = "https://v0.yiketianqi.com/api?unescape=1&version=v63&appid=43656176&appsecret=I42og6Lm&city=平南"
+  if city is None:
+      print('请设置城市')
+      return None
+    url = "https://v0.yiketianqi.com/api?unescape=1&version=v63&appid=43656176&appsecret=I42og6Lm&city=" + city
     res = requests.get(url).json()
     weather = res
     return weather
