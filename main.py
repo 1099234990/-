c021dfe9653540360f642b0d4ab9ce49eb21e812
西米 <111104984+1099234990@users.noreply.github.com>
2023-02-15 10:59:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ data = {
     "color": get_random_color()
   },
   "wind": {
-    "value": str(weather['win'][15]['win_speed']),
+    "value": weather['win']weather['win_speed'],
     "color": get_random_color()
   },
   "air_data": {
